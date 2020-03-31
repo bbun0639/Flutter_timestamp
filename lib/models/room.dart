688971807986469de66f529449dfe45fb467fabf
bbun0639 <pathomphob_sun@cmu.ac.th>
2020-03-31 08:43:13 +0700

    Home page having many list of rooms

---
+++ lib/models/room.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+
+class Room {
+  final String title;
+  //final DateTime startDate;
+  //final DateTime endDate;
+  // final String subject;
+  // final String section;
+  final String building;
+  final String ts;
+  //final String roomType;
+
+  //Constructor yo initialize a product
+  Room(
+    //use curly brances {} to add arguments as named arguments
+     this.title,
+     //this.startDate,
+     //this.endDate,
+     this.building,
+     this.ts,
+     //this.roomType,    
+  );
+}
---
+++ lib/widgets/room_index.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_appgit/screens/rooms_overview_screen.dart';
+import '../models/room.dart';
+import '../widgets/room_index.dart';
+
+// class RoomIndex extends StatelessWidget {
+//   final String id;
+//   final String subject;
+//   final String section;
+//   final String building;
+//   final int roomNum;
+//   final String timeStamp;
+
+//   //Provide the constructor here
+//   RoomIndex(this.id, this.subject, this.section, this.building, this.roomNum, this.timeStamp);
+//}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    RoomsView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TS - Timestamp"),
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'User01@gmail.com',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Dashboard'),
+              leading: Icon(Icons.dashboard),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('My Rooms'),
+              leading: Icon(Icons.home),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Notifications'),
+              leading: Icon(Icons.notifications_active),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: onTabTapped,
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.home),
+              title: new Text("Home"),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.location_on),
+              title: new Text("Map"),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.history),
+              title: new Text("Reported"),
+            ),
+          ]),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
+
+//**Remember ListTile used in ListView - GridTile built-in widget which can be use anywhere*/
