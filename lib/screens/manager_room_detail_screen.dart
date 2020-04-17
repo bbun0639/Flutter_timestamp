622945c107e6b78c2353f248fd7a1ab6dd3f7056
bbun0639 <pathomphob_sun@cmu.ac.th>
2020-04-18 04:05:21 +0700

    separate card GridTile view  and card can onTap one by one

--- lib/screens/manager_room_detail_screen.dart
+++ lib/screens/manager_room_detail_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_appgit/widgets/app_drawer.dart';
 
-
 class ManagerRoomDetailScreen extends StatefulWidget {
   @override
   _ManagerRoomDetailScreen createState() => _ManagerRoomDetailScreen();
@@ -12,7 +11,6 @@ class ManagerRoomDetailScreen extends StatefulWidget {
 class _ManagerRoomDetailScreen extends State<ManagerRoomDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: Text('TS - Timestamp'),
@@ -107,7 +105,6 @@ class _ManagerRoomDetailScreen extends State<ManagerRoomDetailScreen> {
                               Spacer(),
                             ]),
                           ),
-                          
                         ],
                       ),
                     ),
@@ -119,10 +116,235 @@ class _ManagerRoomDetailScreen extends State<ManagerRoomDetailScreen> {
                     //crossAxisSpacing: 10,
                     //mainAxisSpacing: 10,
                     children: <Widget>[
-                      MenuIcon(Icons.update, 'Setting Time', Colors.blue, 'Set the time schedule'),
-                      MenuIcon(Icons.vpn_key, 'Room Key', Colors.red, 'Share code to invite people into this room'),
-                      MenuIcon(Icons.people, 'Users', Colors.teal, 'See all user in this room'),
-                      MenuIcon(Icons.assignment, 'Report', Colors.amber, 'The reported sheet of every user'),
+                      //<----------------------------------->
+                      Card(
+                        elevation: 6.0,
+                        margin: EdgeInsets.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Material(
+                                        color: Colors.blue,
+                                        shape: CircleBorder(),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: Icon(
+                                            Icons.update,
+                                            size: 30.0,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                          padding:
+                                              EdgeInsets.only(bottom: 10.0)),
+                                      Text(
+                                        'Setting Time',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 22.0,
+                                        ),
+                                      ),
+                                      Text('Set the time schedule',
+                                          style:
+                                              TextStyle(color: Colors.black45)),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      //<----------------------------------->
+
+                      Card(
+                        elevation: 6.0,
+                        margin: EdgeInsets.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Material(
+                                        color: Colors.red,
+                                        shape: CircleBorder(),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: Icon(
+                                            Icons.vpn_key,
+                                            size: 30.0,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                          padding:
+                                              EdgeInsets.only(bottom: 10.0)),
+                                      Text(
+                                        'Room Key',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 22.0,
+                                        ),
+                                      ),
+                                      Text(
+                                          'Share code to invite people into this room',
+                                          style:
+                                              TextStyle(color: Colors.black45)),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      //<----------------------------------->
+
+                      Card(
+                        elevation: 6.0,
+                        margin: EdgeInsets.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Material(
+                                        color: Colors.teal,
+                                        shape: CircleBorder(),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: Icon(
+                                            Icons.people,
+                                            size: 30.0,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                          padding:
+                                              EdgeInsets.only(bottom: 10.0)),
+                                      Text(
+                                        'Room Key',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 22.0,
+                                        ),
+                                      ),
+                                      Text(
+                                          'See all user in this room',
+                                          style:
+                                              TextStyle(color: Colors.black45)),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      //<----------------------------------->
+
+                      Card(
+                        elevation: 6.0,
+                        margin: EdgeInsets.all(10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Material(
+                                        color: Colors.amber,
+                                        shape: CircleBorder(),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: Icon(
+                                            Icons.assignment,
+                                            size: 30.0,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                          padding:
+                                              EdgeInsets.only(bottom: 10.0)),
+                                      Text(
+                                        'Report',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 22.0,
+                                        ),
+                                      ),
+                                      Text('The reported sheet of every user',
+                                          style:
+                                              TextStyle(color: Colors.black45)),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      // MenuIcon(Icons.update, 'Setting Time', Colors.blue,
+                      //     'Set the time schedule'),
+                      // MenuIcon(Icons.vpn_key, 'Room Key', Colors.red,
+                      //     'Share code to invite people into this room'),
+                      // MenuIcon(Icons.people, 'Users', Colors.teal,
+                      //     'See all user in this room'),
+                      // MenuIcon(Icons.assignment, 'Report', Colors.amber,
+                      //     'The reported sheet of every user'),
                     ],
                   ),
                 ),
@@ -135,16 +357,14 @@ class _ManagerRoomDetailScreen extends State<ManagerRoomDetailScreen> {
   }
 }
 
-
-
-
 class MenuIcon extends StatelessWidget {
   final IconData _icon;
   final String _title;
   final MaterialColor _color;
   final String _descripe;
+  final Function onClick;
 
-  MenuIcon(this._icon, this._title, this._color, this._descripe);
+  MenuIcon(this._icon, this._title, this._color, this._descripe, this.onClick);
 
   @override
   Widget build(BuildContext context) {
